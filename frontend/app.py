62c047bb74e9f50310be1b63fa6b304451407c55
Jennifer Mota <jmota@fi.uba.ar>
2024-11-06 12:58:17 -0300

    #1 tarjetas de perros perdidos y banner

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,11 @@
 import requests
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def index():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
