563482e7b24bfe3d0a4f69b2d42ae9dde616b7e5
f3d3to <fneuman@fi.uba.ar>
2024-11-07 17:46:56 -0300

    Agrego ajustes a estilos de todo el proyecto

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/cargarMascota')
+@app.route('/cargarMascota/')
 def cargar_mascota():
     return render_template('cargarMascotaPerdida.html')
 
