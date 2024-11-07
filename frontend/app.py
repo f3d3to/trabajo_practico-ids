1f5cf56b3983940e03ad562208145d6686c3d2e3
Gabyo <gururi@fi.uba.ar>
2024-11-06 22:22:09 -0300

    11 Se actualizan templates de Contacto. Trello id#11

--- frontend/app.py
+++ frontend/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def cargar_mascota():
     return render_template('cargarMascotaPerdida.html')
 
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
 @app.route('/')
 def index():
     return render_template('home.html')
